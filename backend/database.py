1efea16505dc80237543328a8636fdf4763bb259
NandhiniRavi01 <113678413+NandhiniRavi01@users.noreply.github.com>
2026-01-03 12:12:08 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -25,6 +25,8 @@ class Database:
                 user=self.user,
                 password=self.password,
                 database=self.database,
+                ssl_ca="/etc/ssl/certs/rds-ca.pem",   # <- always exists in container
+                ssl_verify_cert=True
                 
             )
             return connection
